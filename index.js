d3c9d3c3205226b2c23f7aa59e182c2156cbb25e
Sterling Moss <mtsilvertm@gmail.com>
2018-06-01 12:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  obj.key = value;
-  return obj;
+  object.key = value;
+  return object;
 }
